aa6f36755a1ae77ed9ca7c72a2b93c13bdc97c64
raselpy <rasel.py@gmail.com>
2020-05-22 11:06:15 +0600

    Updates

---
+++ precache-manifest.4a65154ca3e63f15735bd497c0397766.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "157314f8394936e61062f845f5e8ac6f",
+    "url": "/movieDatabase/index.html"
+  },
+  {
+    "revision": "9def03edf99137777a8c",
+    "url": "/movieDatabase/static/css/main.f95a285c.chunk.css"
+  },
+  {
+    "revision": "bfe0e7025668f992161a",
+    "url": "/movieDatabase/static/js/2.2c5869c0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/movieDatabase/static/js/2.2c5869c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9def03edf99137777a8c",
+    "url": "/movieDatabase/static/js/main.0e2cec14.chunk.js"
+  },
+  {
+    "revision": "feb0c98bc36ac94ed967",
+    "url": "/movieDatabase/static/js/runtime-main.c56f9d47.js"
+  }
+]);
